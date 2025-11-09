6db5f9488b9fb94207bd63f963d6838a5d7e5850
AhmadIqbalFE <a.iqbal27112005@gmail.com>
2025-11-09 12:28:44 +0700

    Tugas Alpro
    Tugas Pratikum Alpro

---
+++ Variabel.java
@@ -0,0 +1,21 @@
+public class Variabel {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        String namaLengkap = "Ahmad Iqbal";
+        String tempatTanggalLahir = "Jakarta, 10 Januari 2000";
+        String jenisKelamin = "Laki-laki";
+        String alamatLengkap = "Jl. Merdeka No. 123, Jakarta";
+        String noHp = "081234567890";
+        double penghasilanSebulan = 5000000.00;
+
+        // Output ke layar
+        System.out.println("Assalamu’alaikum");
+        System.out.println("Izinkan saya memperkenalkan diri");
+        System.out.println("Nama Lengkap        : " + namaLengkap);
+        System.out.println("Tempat & Tanggal Lahir : " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin       : " + jenisKelamin);
+        System.out.println("Alamat Lengkap      : " + alamatLengkap);
+        System.out.println("No Hp               : " + noHp);
+        System.out.println("Penghasilan Sebulan : Rp" + penghasilanSebulan);
+    }
+}
